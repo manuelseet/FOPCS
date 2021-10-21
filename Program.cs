a948c6d68b4fd25b966e6e73fa882f6cd34690fd
Manuel Seet <e0838385@u.nus.edu>
2021-10-21 11:38:47 +0800

    Added multidim array

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace FOPCS_Revision
     {
         static void Main(string[] args)
         {
-        }
+
+			int[,] arr2D = new int[,] { { 35, 82 }, { 67, 45 }, { 62, 77 } };
+
+			for (int i = 0; i < arr2D.GetLength(0); i++)
+			{
+				for (int j = 0; j < arr2D.GetLength(1); j++)
+				{
+					Console.WriteLine($"In row {i}, {j}-th elem is {arr2D[i,j]}");
+				}
+			}
+		}
     }
 }
