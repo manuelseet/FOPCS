7d14ed512bd9eee47b59a05e95c6eef05c1c8bd9
Manuel Seet <e0838385@u.nus.edu>
2021-10-21 21:36:45 +0800

    ExerciseB

--- ExerciseB.cs
+++ ExerciseB.cs
@@ -10,6 +10,47 @@ namespace FOPCS_Revision
     {
         static void Main(string[] arg)
         {
+            /*
+            //1
+            Console.Write("Enter a double precision number: ");
+            double uNum1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"{Math.Sqrt(uNum1)}");
+
+            //2
+            Console.Write("Enter a double precision number: ");
+            double uNum2 = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"{Math.Sqrt(uNum2): #.000}");
+
+            //3
+            Console.Write("Enter a salary: ");
+            double salaryPlus = Convert.ToDouble(Console.ReadLine());
+            salaryPlus = salaryPlus * 1.13;
+            Console.WriteLine(salaryPlus.ToString("C2"));
+
+            //4
+            Console.Write("Enter a temperature in Centigrade: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            double F = celsius * 1.8 + 32;
+            Console.WriteLine($"{celsius} C = {F} F");
+
+            //5 - skip
+
+            */
+            //6 - se String split
+            Console.Write("Please enter 4 digits (x1, x2, y1, y2), separated by commas/spaces: ");
+            string[] strSplit = Console.ReadLine().Split(new char[] { ',', ' '});
+            double[] nums = new double[4];
+            
+            
+            int j = 0;
+            foreach (string i in strSplit)
+            {
+                nums[j] = Convert.ToDouble(i);
+                Console.WriteLine(i);
+            }
+
+            double distance = Math.Sqrt(Math.Pow((nums[1] - nums[0]), 2) + Math.Pow((nums[3] - nums[2]), 2));
+            Console.WriteLine("distance = {0}", distance);
 
         }
 
